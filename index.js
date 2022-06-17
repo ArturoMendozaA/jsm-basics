37416ee971b77c3c3888aeb34e13ba9f507c1051
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-06-16 22:17:26 -0500

    Add variable section stuff

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Hello, World');
+
+ let names = "Mash smash"
+ console.log(names);
+ let nameUndefined;
+ console.log(nameUndefined);
+ nameUndefined = "pancake"
+ console.log(nameUndefined)
+
+ 
