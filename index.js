5fe8c765b1c7223079c836c72e0900173e0afa8c
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-06-20 11:46:36 -0500

    Add variable examples

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ console.log('Hello, World');
  nameUndefined = "pancake"
  console.log(nameUndefined)
 
+// using let 
+//  let varRate = 1
+//  varRate = 2
+//  console.log(varRate)
  
+//using const
+
+ const varRate = 1
+// //  varRate = 2
+ console.log(varRate)
+ 
+ let person = {firstName: "Cucamonga", lastName:"harnandez", age: 30}
+ console.log(person)
